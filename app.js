7c6d40e12a548a1c0969a7dea89340b46e75196c
ZARA <zahraalemi@gmail.com>
2023-04-01 18:20:15 +0200

    Add style

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ let playerArray = [];
 const nextBtn = document.querySelector(".next-btn");
 const mafiaGroupchk = Array.from(document.getElementsByName('mafia-group'));
 const simpleMafia = document.querySelector('.simple-mafia');
-const citizenGroupchk = Array.from(document.getElementsByName('citizen-group'));
-const simpleCitizen = document.querySelector('.simple-citizen');
-const formBox = document.querySelector('.form-box');
+const townsGroupchk = Array.from(document.getElementsByName('towns-group'));
+const townsPeople = document.querySelector('.towns-people');
+const mainShowcase = document.querySelector('.main-showcase');
 const resultBox = document.querySelector('.result-box');
 const descriptionPlayer = document.querySelector('.description-player');
 const startBtn = document.querySelector('.start-btn')
+const reStartBtn = document.querySelector('.restart-btn')
 const popUpBox = document.querySelector('.popup-box')
 const roleCard = document.querySelector('.role-card')
 const doneBtn = document.querySelector('.done-btn')
@@ -24,25 +25,27 @@ nextBtn.addEventListener("click", function () {
   const mafia = Number(simpleMafia.value)
   if (mafia > 0) {
     for (let i = 0; i < mafia; i++) {
-      playerArray.push("mafia sade")
+      playerArray.push("mafia")
     }
   }
-  citizenGroupchk.forEach(citizen => {
-    if (citizen.checked) {
-      playerArray.push(citizen.id)
+  townsGroupchk.forEach(towns => {
+    if (towns.checked) {
+      playerArray.push(towns.id)
     }
   });
 
-  const citizen = Number(simpleCitizen.value)
-  if (citizen > 0) {
-    for (let i = 0; i < citizen; i++) {
-      playerArray.push("citizen sade")
+  const towns = Number(townsPeople.value)
+  if (towns > 0) {
+    for (let i = 0; i < towns; i++) {
+      playerArray.push("townspeople")
     }
   }
 
-  console.log(playerArray)
-  formBox.classList.add("hidden-box");
+  /* console.log(playerArray) */
+  mainShowcase.classList.add("hidden-box");
+  mainShowcase.classList.remove("main-showcase");
   resultBox.classList.remove("hidden-box");
+  resultBox.classList.add("show-box");
   descriptionPlayer.innerHTML = `<h4>Total player ${playerArray.length} <br/>
 Now you can start game </h4><p>After seeing your role, don't forget to press the done button</p>`
 });
@@ -55,7 +58,10 @@ startBtn.addEventListener('click', function () {
     if (!randomArray.includes(role)) {
       randomArray.push(role)
       popUpBox.classList.remove('hidden-box')
-      roleCard.innerHTML = `<h1>${playerArray[role]}</h1>`
+      popUpBox.classList.add('show-box')
+      roleCard.innerHTML = `
+      <div class="${playerArray[role]}-icon role-icon"></div>
+      <h1>${playerArray[role]}</h1>`
     }
   }
 
@@ -63,15 +69,20 @@ startBtn.addEventListener('click', function () {
   console.log(randomArray)
 })
 
+reStartBtn.addEventListener('click',function(){
+  window.location.reload();
+})
+
 const randomFunction = function () {
   const randomNumber = Math.floor(Math.random() * playerArray.length);
   return randomNumber
 }
 
-doneBtn.addEventListener('click',function(){
+doneBtn.addEventListener('click', function () {
   popUpBox.classList.add('hidden-box');
-  
+  popUpBox.classList.remove('show-box');
+
   if (randomArray.length === playerArray.length) {
-    startBtn.disabled ='true';
+    startBtn.disabled = 'true';
   }
 })
